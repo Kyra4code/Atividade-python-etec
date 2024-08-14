caa2dcef1ccfe8979441bfbbfe800d6b4431e0de
Kyra4code <luacrioecharmito@gmail.com>
2024-08-14 16:46:34 -0300

    correcao

---
+++ Atividade dia 13.08/Atividade02.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade:")
+peso = input("Digite seu peso: ")
+cidade = input("Digite sua cidade: ")
+
+print(f'Olá, Meu nome é {nome}, eu tenho {idade} anos, meu peso é {peso} e moro em {cidade}')
---
+++ Atividade dia 13.08/atividade01.py
@@ -0,0 +1,3 @@
+print("Hello World\n")
+print("Estou Aprendendo python\n")
+print("Opção escolhida com sucesso\n")
