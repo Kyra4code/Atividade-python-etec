5cfdbefd7b73878e3376237e5c5be5f55ed6c851
Kyra4code <lucarioecharmito@gmail.com>
2024-09-17 22:28:19 -0300

    Correcoes-no-codigo

--- Nova pasta/atividade07.py
+++ Nova pasta/atividade07.py
@@ -1,12 +1,12 @@
 def main():
-    x = input("Insira o primeiro valor: ")
-    y = input("Insira o segundo valor: ")
-    z = input("Insira o terceir valor: ")
+    x = int(input("Insira o primeiro valor: "))
+    y = int(input("Insira o segundo valor: "))
+    z = int(input("Insira o terceiro valor: "))
 
     if x > y and z:
         print(f"O primeiro valor inserido {x} é maior")
     elif y > x and z:
-        print(f"O segudo valor inserido {y} é maior")
+        print(f"O segundo valor inserido {y} é maior")
     elif z > x and y:
         print(f"O terceiro numero inserido {z} é maior")
     else:
--- Nova pasta/atividade5.py
+++ Nova pasta/atividade5.py
@@ -1,8 +1,10 @@
 def main():
     salario = float(input("Insira seu salário: "))
     desconto = float(input("Insira seu desconto(%): "))
-    calcdes = (desconto / 100) * -salario + salario
+    calcdes = (desconto / 100) * salario
+    salariof = salario -calcdes
 
-    print(f"Seu desconto é de {calcdes}") 
+    print(f"Seu salario no final é {salariof}")
+    print(f"Seu desconto é de {calcdes}")
 
 main()
