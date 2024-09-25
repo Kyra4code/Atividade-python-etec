319997a64fc6b81e7037ef0b0abb73cb35fddca0
Kyra4code <lucarioecharmito@gmail.com>
2024-09-24 21:57:16 -0300

    correcao-da-atividade07

--- Nova pasta/atividade07.py
+++ Nova pasta/atividade07.py
@@ -3,11 +3,11 @@ def main():
     y = int(input("Insira o segundo valor: "))
     z = int(input("Insira o terceiro valor: "))
 
-    if x > y and z:
+    if x > y and x > z:
         print(f"O primeiro valor inserido {x} é maior")
-    elif y > x and z:
+    elif y > x and y > z:
         print(f"O segundo valor inserido {y} é maior")
-    elif z > x and y:
+    elif z > x and z > y:
         print(f"O terceiro numero inserido {z} é maior")
     else:
         print("Existe alguns numeros iguais")
