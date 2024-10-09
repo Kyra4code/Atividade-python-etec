0f1072d5a597b54a52c0aa747f86d9c7283894e2
Kyra4code <lucarioecharmito@gmail.com>
2024-10-08 22:19:27 -0300

    atividade-de-fixacao

--- Atividade lista/atividade.py
+++ Atividade lista/atividade.py
@@ -1,24 +1,96 @@
-lista = ["Banana", "melancia", "abobora"]
+# def atividade1():
+#     lista = ["1,2,3,4,5,6,,7,8,9,10"]
+#     print(lista)
 
-def appendInsert():
-    print(lista)
-    lista.append("amendoim")
-    print(lista)
-    lista.remove("amendoim")
-    lista.insert(3, "amendoim")
-    print(lista)
+# #atividade1()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade2():
+#         lista = [2,3]
+#         soma = lista[0] + lista[1]
+#         print(soma)
+
+# atividade2()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade3():
+
+#     lista = ['pera','abacaxi', 'morango','laranja','uva']
+#     i = 0
+#     for i in range(len(lista)):
+#         if(lista[i] == "morango"):
+#             print("tem morango")
+#         else:
+#             print("nao tem morango")
+
+# atividade3()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade4():
+#     lista = [1,1,1,1,1,1]
+#     print(lista.count(1))
     
-appendInsert()
+# atividade4()
 
+#----------------------------------------------------------------------------------------------------------------------------------
 
-def removeDel():
+# def atividade5():
+#     lista = ['Ana', 'Bruno', 'Carlos', 'Daniela']
+#     lista.remove('Carlos')
+#     print(lista)
+
+# atividade5()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividae6():
+#     lista = [1,2,3]
+#     lista2 = [4,5,6]
+
+#     # lista.append(lista2) ou
+#     print(lista + lista2)
+
+# atividae6()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade7():
+#     lista = ["abacaxi", 'pera', 'uva', 'maca', 'melancia', 'kiwi']
+#     print(f'antes: {lista}')
+#     lista.append('pitaya')
+#     lista.append('banana')
+#     print(f'depois: {lista}')
+
+# atividade7()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade8():
+#     lista = ['abacaxi', 'pera', 'uva', 'maca', 'melancia']
+#     lista[0] = lista[1]
+#     lista.append('abacaxi')
+#     lista.remove(lista[1])
+#     print(lista)
+
+# atividade8()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# def atividade9():
+#     lista = ['abacaxi', 'pera', 'uva', 'maca', 'melancia', 'kiwi', 'pitaya', 'banana']
+#     lista.insert(0, 'melao')
+#     print(lista)
+
+# atividade9()
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def atividade10():
+    lista = ['abacaxi', 'pera', 'uva', 'maca', 'melancia']
+    del(lista[3])
     print(lista)
-    lista.remove("Banana")
-    print(lista)
-    lista.insert(0, "Banana")
-    del(lista[0])
-    print(lista)
 
-removeDel()
-
-lista2 = ("um", "dois", "tres")
+atividade10()
